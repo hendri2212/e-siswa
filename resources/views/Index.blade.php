1e83885a95466aa41bceb53c64375eebedadbe5b
hendri2212 <cowok_cool320@yahoo.co.id>
2023-08-31 15:06:55 +0800

    example use bootstrap

--- resources/views/Index.blade.php
+++ resources/views/Index.blade.php
@@ -6,21 +6,43 @@
     <title>e-Siswa</title>
     @vite(['resources/js/app.js'])
 </head>
-<body class="bg-info">
-    <a href="/guru">Data Guru</a>
-    <a href="/siswa">Data Siswa</a>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    
-    
-    @yield('title')
-    
-    <br><br><br>
-    
+<body class="bg-secondary">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container">
+            <a class="navbar-brand" href="#">App e-Siswa</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/guru">Data Guru</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/siswa">Data Siswa</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
     <div class="container">
-        @yield('content')
+        <div class="row">
+            <div class="card mt-3">
+                <div class="card-body">
+                    <h5 class="card-title">@yield('title')</h5>
+                    <p class="card-text">
+                        @yield('content')
+                    </p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
     </div>
 </body>
 </html>
