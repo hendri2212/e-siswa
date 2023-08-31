d9f070c15b6a951c0e38f82d35e5273d8f30deb9
hendri2212 <cowok_cool320@yahoo.co.id>
2023-08-31 14:56:35 +0800

    add bootstrap

--- resources/views/Index.blade.php
+++ resources/views/Index.blade.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>e-Siswa</title>
+    @vite(['resources/js/app.js'])
 </head>
-<body>
+<body class="bg-info">
     <a href="/guru">Data Guru</a>
     <a href="/siswa">Data Siswa</a>
     <br/>
